2cff28d567bcd328023a01a2eaf7b89391379bc6
EDUAR DORADO <eduar.dorado@telefonica.com>
2022-06-16 18:18:17 -0500

    adding service workers

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const OptimizeCSSAssetsPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-// const WorkboxPlugin = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     mode: 'production',
@@ -42,6 +42,6 @@ module.exports = {
           filename: "./index.html", 
       }),
       new MiniCssExtractPlugin({ filename: "[name].css" }),
-    //   new WorkboxPlugin.GenerateSW(),
+      new WorkboxPlugin.GenerateSW(),
     ]
 }
